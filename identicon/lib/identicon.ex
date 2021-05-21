8ec4d59528f58f4070589857ac73b5dcb73dc389
chrisoney <christopher.oney@yahoo.com>
2021-05-20 22:15:30 -0700

    get rgb values for identicon

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -4,15 +4,27 @@ defmodule Identicon do
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Identicon.hello()
-      :world
-
+  The main part of this module that runs all of the other functions
   """
-  def hello do
-    :world
+
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
   end
+
+  def pick_color(image) do
+    %Identicon.Image{hex: [r, g, b | _tail]} = image
+
+    [r, g, b]
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
+
+  
 end
---
+++ identicon/lib/image.ex
@@ -0,0 +1,8 @@
+defmodule Identicon.Image do 
+  @moduledoc """
+  Holds the Image struct for the data in this application.
+  """
+
+  defstruct hex: nil
+  
+end
