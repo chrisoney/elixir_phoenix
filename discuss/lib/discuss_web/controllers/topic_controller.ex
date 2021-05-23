f08e1662b120d7564483d7ffd4f2ff801c503dab
chrisoney <christopher.oney@yahoo.com>
2021-05-22 23:24:04 -0700

    add error messages to forms

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -2,14 +2,22 @@ defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
 
   alias DiscussWeb.Topic
+  alias Discuss.Repo
 
-  def new(conn, params) do
+  def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
 
     render conn, "new.html", changeset: changeset
   end
 
-  def create(conn, params) do
-    IO.inspect(params)
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, post} -> IO.inspect(post)
+      {:error, changeset} -> 
+        render conn, "new.html", changeset: changeset
+    end
+
   end
 end
