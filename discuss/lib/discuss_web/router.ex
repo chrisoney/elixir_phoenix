af5d4ae751c1b2b2122fe1364fcb8256df38b065
chrisoney <christopher.oney@yahoo.com>
2021-05-23 22:41:22 -0700

    set up routing for oauth with github

--- discuss/lib/discuss_web/router.ex
+++ discuss/lib/discuss_web/router.ex
@@ -26,6 +26,13 @@ defmodule DiscussWeb.Router do
     resources "/", TopicController
   end
 
+  scope "/auth", DiscussWeb do
+    pipe_through :browser
+
+    get "/:provider", AuthController, :request
+    post "/:provider/callback", AuthController, :callback
+  end
+
   # Other scopes may use custom stacks.
   # scope "/api", DiscussWeb do
   #   pipe_through :api
