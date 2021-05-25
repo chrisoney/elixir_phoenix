a53c140111c8c0b0c8e2a657fde556e5700c95b7
chrisoney <christopher.oney@yahoo.com>
2021-05-24 21:53:00 -0700

    start SetUser plug

---
+++ discuss/lib/discuss_web/controllers/plugs/set_user.ex
@@ -0,0 +1,16 @@
+defmodule DiscussWeb.Plugs.SetUser do
+  import Plug.Conn
+  import Phoenix.Controller
+
+  alias Discuss.Repo
+  alias DiscussWeb.User
+  alias DiscussWeb.Router.Helpers
+
+  def init(_params) do
+
+  end
+
+  def call(conn, _params) do
+
+  end
+end
