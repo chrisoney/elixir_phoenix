f67e2f8b53c64f3cce29db19950db424ce4cde2f
chrisoney <christopher.oney@yahoo.com>
2021-05-26 21:38:14 -0700

    progress on set_user plug

--- discuss/lib/discuss_web/controllers/plugs/set_user.ex
+++ discuss/lib/discuss_web/controllers/plugs/set_user.ex
@@ -7,10 +7,17 @@ defmodule DiscussWeb.Plugs.SetUser do
   alias DiscussWeb.Router.Helpers
 
   def init(_params) do
-
   end
 
   def call(conn, _params) do
+    user_id = get_session(conn, :user_id)
 
+    cond do
+      user = user_id && Repo.get(User, user_id) ->
+        assign(conn, :user, user)
+        # conn.assigns.user => user struct
+      true ->
+
+    end
   end
 end
