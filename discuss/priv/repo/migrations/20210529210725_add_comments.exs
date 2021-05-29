735b904383ddc11621ef9294c8b7e17509a653b1
chrisoney <christopher.oney@yahoo.com>
2021-05-29 14:12:37 -0700

    fix create table invocation

--- discuss/priv/repo/migrations/20210529210725_add_comments.exs
+++ discuss/priv/repo/migrations/20210529210725_add_comments.exs
@@ -2,7 +2,7 @@ defmodule Discuss.Repo.Migrations.AddComments do
   use Ecto.Migration
 
   def change do
-    create_table(:Comments) do
+    create table(:Comments) do
       add :content, :text
       add :user_id, references(:Users)
       add :topic_id, references(:Topics)
